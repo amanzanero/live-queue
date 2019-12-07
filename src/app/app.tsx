374389f98a03cb1441df8c7ddee32df06635be98
Andrew Manzanero <manzanero.andrew@gmail.com>
2019-12-07 13:55:05 -0800

    mui and package-lock stuff

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,8 +1,16 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
+import { styled } from "@material-ui/core/styles";
 
 const App: React.FC = () => {
   return <div>Hello world!</div>;
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const AppWrapper = styled(App)({
+  body: {
+    margin: 0,
+    fontFamily: "Roboto Slab"
+  }
+});
+
+ReactDOM.render(<AppWrapper />, document.getElementById("root"));
