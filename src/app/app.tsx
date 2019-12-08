8d0876e115c3c2a5ca7edc73ca0b2a778753f68b
Andrew Manzanero <manzanero.andrew@gmail.com>
2019-12-08 14:31:55 -0800

    update theming

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,33 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
+import Navbar from "./components/Navbar";
 import "./app.css";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#00838f",
+      light: "#4fb3bf",
+      dark: "#005662"
+    },
+    secondary: {
+      main: "#303f9f",
+      light: "#666ad1",
+      dark: "#001970"
+    }
+  },
+  spacing: factor => `${0.25 * factor}rem` // (Bootstrap strategy)
+});
+
 const App: React.FC = () => {
-  return <div>Hello world!</div>;
+  return <Navbar />;
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
